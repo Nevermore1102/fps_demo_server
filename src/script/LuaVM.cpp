5b2893762c2a6b0e043412fbe3e3604a547a61eb
Nevermore1102 <1535332051@qq.com>
2025-05-09 00:43:36 +0800

    feat: 完善 Lua 虚拟机模块 - 解决编译错误

--- src/script/LuaVM.cpp
+++ src/script/LuaVM.cpp
@@ -1,40 +1,113 @@
-#include "LuaVM.h"
-#include <iostream>
+#include "script/LuaVM.h"
+#include <stdexcept>
 
 LuaVM::LuaVM() : L_(nullptr) {}
 
 LuaVM::~LuaVM() {
-    close();
+    if (L_) {
+        lua_close(L_);
+        L_ = nullptr;
+    }
 }
 
 bool LuaVM::init() {
     L_ = luaL_newstate();
     if (!L_) {
-        std::cerr << "Failed to create Lua state" << std::endl;
         return false;
     }
     
     luaL_openlibs(L_);
+    registerBaseFunctions();
     return true;
 }
 
-bool LuaVM::loadScript(const std::string& path) {
+bool LuaVM::loadScript(const std::string& filename) {
     if (!L_) {
-        std::cerr << "Lua state not initialized" << std::endl;
         return false;
     }
-
-    if (luaL_dofile(L_, path.c_str()) != 0) {
-        std::cerr << "Failed to load Lua script: " << lua_tostring(L_, -1) << std::endl;
+    
+    if (luaL_loadfile(L_, filename.c_str()) || lua_pcall(L_, 0, 0, 0)) {
+        handleError("Failed to load script: " + filename);
         return false;
     }
-
+    
+    loadedScripts_[filename] = filename;
     return true;
 }
 
-void LuaVM::close() {
-    if (L_) {
-        lua_close(L_);
-        L_ = nullptr;
+bool LuaVM::reloadScript(const std::string& filename) {
+    if (loadedScripts_.find(filename) == loadedScripts_.end()) {
+        return false;
     }
+    
+    return loadScript(filename);
+}
+
+void LuaVM::registerFunction(const std::string& name, lua_CFunction func) {
+    if (!L_) {
+        return;
+    }
+    
+    lua_pushcfunction(L_, func);
+    lua_setglobal(L_, name.c_str());
+}
+
+bool LuaVM::callFunction(const std::string& funcName, const char* format, ...) {
+    if (!L_) {
+        return false;
+    }
+    
+    lua_getglobal(L_, funcName.c_str());
+    if (!lua_isfunction(L_, -1)) {
+        lua_pop(L_, 1);
+        return false;
+    }
+    
+    va_list args;
+    va_start(args, format);
+    
+    int nargs = 0;
+    for (const char* ptr = format; *ptr; ++ptr) {
+        switch (*ptr) {
+            case 'i':
+                lua_pushinteger(L_, va_arg(args, int));
+                break;
+            case 'n':
+                lua_pushnumber(L_, va_arg(args, double));
+                break;
+            case 's':
+                lua_pushstring(L_, va_arg(args, const char*));
+                break;
+            case 'b':
+                lua_pushboolean(L_, va_arg(args, int));
+                break;
+            default:
+                va_end(args);
+                return false;
+        }
+        ++nargs;
+    }
+    
+    va_end(args);
+    
+    if (lua_pcall(L_, nargs, 0, 0) != 0) {
+        handleError("Failed to call function: " + funcName);
+        return false;
+    }
+    
+    return true;
+}
+
+void LuaVM::handleError(const std::string& msg) {
+    if (L_) {
+        const char* error = lua_tostring(L_, -1);
+        if (error) {
+            throw std::runtime_error(msg + "\n" + error);
+        }
+        lua_pop(L_, 1);
+    }
+}
+
+void LuaVM::registerBaseFunctions() {
+    // TODO: 注册基础函数
 } 
--- src/script/LuaVM.h
+++ src/script/LuaVM.h
@@ -17,18 +17,33 @@
 #include <lua.hpp>
 #include <string>
 #include <memory>
+#include <unordered_map>
 
 class LuaVM {
 public:
     LuaVM();
     ~LuaVM();
-
+    
     bool init();
-    bool loadScript(const std::string& path);
-    void close();
-
-    lua_State* getState() const { return L_; }
-
+    bool loadScript(const std::string& filename);
+    bool reloadScript(const std::string& filename);
+    
+    // 注册 C++ 函数到 Lua
+    void registerFunction(const std::string& name, lua_CFunction func);
+    
+    // 调用 Lua 函数
+    bool callFunction(const std::string& funcName, const char* format, ...);
+    
+    // 获取 Lua 状态
+    lua_State* getState() { return L_; }
+    
 private:
     lua_State* L_;
+    std::unordered_map<std::string, std::string> loadedScripts_;
+    
+    // 错误处理
+    void handleError(const std::string& msg);
+    
+    // 注册基础函数
+    void registerBaseFunctions();
 }; 
